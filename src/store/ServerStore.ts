7d3a9829ee6a00944afe49daa616ab007d8b8ffb
anhchangvt1994 <anhchangvt1994@gmail.com>
2024-06-02 00:05:41 +0700

    update(other): :jigsaw: update
    update ServerStore.ts

--- src/store/ServerStore.ts
+++ src/store/ServerStore.ts
@@ -42,25 +42,7 @@ export let BotInfo: IBotInfo
 
 export let DeviceInfo: IDeviceInfo
 
-export let LocaleInfo: ILocaleInfo = {
-	lang: '',
-	country: '',
-	clientLang: '',
-	clientCountry: '',
-	defaultLang: '',
-	defaultCountry: '',
-	langSelected: '',
-	countrySelected: '',
-	hideDefaultLocale: false,
-	range: [0, 0],
-	region: '',
-	eu: '',
-	timezone: '',
-	city: '',
-	ll: [0, 0],
-	metro: 0,
-	area: 0,
-}
+export let LocaleInfo: ILocaleInfo
 
 export const ServerStore = (() => {
 	const html = document.documentElement
@@ -124,7 +106,7 @@ export const ServerStore = (() => {
 				LocaleInfo = (() => {
 					const strInfo = getCookie('LocaleInfo')
 
-					const info = strInfo ? JSON.parse(strInfo) : LocaleInfo
+					const info = strInfo ? JSON.parse(strInfo) : LocaleInfo || {}
 
 					setLocaleState({
 						lang: info.langSelected,
