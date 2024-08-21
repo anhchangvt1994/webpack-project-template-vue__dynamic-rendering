cab7110ad693778e69340ede2b632aa80496e39c
anhchangvt1994 <anhchangvt1994@gmail.com>
2024-08-21 17:09:56 +0700

    update(server): :jigsaw: update
    update pm2 logic handler

--- server/src/puppeteer-ssr/pm2.ts
+++ server/src/puppeteer-ssr/pm2.ts
@@ -32,16 +32,7 @@ pm2.connect(false, (err) => {
 
 			let counter = 0
 			for (const process of processList) {
-				if (process.name === 'cleaner-service' && process.pm_id !== undefined) {
-					// pm2.restart(process.pm_id, function (err) {
-					// 	counter++
-					// 	if (err) {
-					// 		console.error(err)
-					// 		selfProcess.exit(2)
-					// 	}
-					// 	if (counter === totalProcess) resAfterCheckToRestart(true)
-					// })
-				} else if (
+				if (
 					(process.name === 'start-puppeteer-ssr' ||
 						process.name === 'puppeteer-ssr') &&
 					process.pm_id !== undefined
@@ -62,33 +53,12 @@ pm2.connect(false, (err) => {
 		})
 
 		if (!hasRestarted) {
-			// pm2.start(
-			// 	{
-			// 		name: 'cleaner-service',
-			// 		script: `${distPath}/utils/CleanerService.${resourceExtension}`,
-			// 		instances: 1,
-			// 		exec_mode: 'cluster',
-			// 		interpreter: './node_modules/.bin/sucrase',
-			// 		interpreter_args: '--require sucrase/register',
-			// 		wait_ready: true,
-			// 		kill_timeout: CLUSTER_KILL_TIMEOUT,
-			// 		cwd: '.',
-			// 		env: {},
-			// 	},
-			// 	function (err, apps) {
-			// 		if (err) {
-			// 			Console.error(err)
-			// 			return
-			// 		}
-			// 	}
-			// )
-
 			pm2.start(
 				{
 					name: 'puppeteer-ssr',
 					script: `server/${resourceDirectory}/index.${resourceExtension}`,
 					instances: CLUSTER_INSTANCES,
-					exec_mode: CLUSTER_INSTANCES === 1 ? 'fork' : 'cluster',
+					exec_mode: 'cluster',
 					interpreter: './node_modules/.bin/sucrase',
 					interpreter_args: '--require sucrase/register',
 					wait_ready: true,
